e887363e6fc3e53d5b98c28da5b89e89a085de86
JaredLopezAlamilla <56237409+JaredLopezAlamilla@users.noreply.github.com>
2019-10-30 13:53:01 +1300

    Update Diffusion1D_k_space.m

--- Diffusion1D_k_space.m
+++ Diffusion1D_k_space.m
@@ -1,8 +1,3 @@
-Fxs=-50:1:50;
-DDeffs=zeros(1,length(Fxs)); 
-vxxs=zeros(1,length(Fxs)); 
-jj=1; 
-
 M=101; MM=101; L=1; xgrid=deal(L*(0:M-1)/M); 
 Vx=3;Vy=0;Vxy=0; kBTx=1; gx=1; fx=4;
 % ---- Creates potential ----
@@ -13,6 +8,10 @@ Inxnorm=ceil(M/2);
 M=29;n=-floor(M/2):floor(M/2);
 Vk=Vk(1,Inxnorm-floor(M/2):Inxnorm+floor(M/2));
 
+Fxs=-50:1:50;
+DDeffs=zeros(1,length(Fxs)); 
+vxxs=zeros(1,length(Fxs)); 
+jj=1; 
 for j=Fxs
     
     fx=j;    
@@ -23,7 +22,7 @@ for j=Fxs
     mid = round(length(Jxkk)/2); K0=ceil((M)/2);
     vx_k=fftshift(fft(J0)); vx_k=real(vx_k(ceil(M/2)));
     % ---- Solves for diffsuion ---- 
-    diffusion=EffDiff_kspace();        
+    diffusion=EffDiff_kspace(Pk,Dk,Jk,n,K0);        
     vxxs(jj)=vxk;    
     DDeffs(jj)=diffusion;    
     jj=jj+1;
